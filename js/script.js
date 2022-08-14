6299dd4033830c538398914d98f9e206394e4321
MargaritaVin <ritasergienko@rambler.ru>
2022-08-14 21:32:19 +0300

    added class Employee in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+class Employee {
+    static daysVacation = 18;
+    constructor(firstName, lastName, age, jobPosition, salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.jobPosition = jobPosition;
+        this.salary = salary;
+    }
+    get fullName() {
+        console.log(`${this.firstName} ${this.lastName}`);
+    }
+    yearSalary() {
+        console.log (this.salary * 12);
+    } 
+};
+const manager = new Employee("Peter", "Parker", 23, "manager", 10000);
+const accountant = new Employee("Anna", "Karenina", 31, "accountant", 20000);
+
+console.log(manager);
+console.log(accountant);
+manager.fullName;
+accountant.fullName;
+manager.yearSalary();
+accountant.yearSalary();
